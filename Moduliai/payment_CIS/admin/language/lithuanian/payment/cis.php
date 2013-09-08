4d3fe285dd65bed6c440a79e82ace60c6d1f5050
Andrejus <as@andrejus.eu>
2013-09-08 17:31:44 +0300

    Apmokėjimo modulis CIS
    CIS - Cash In Shop, apmokėjimas atsiėmimo metu parduotuvėje.

---
+++ Moduliai/payment_CIS/admin/language/lithuanian/payment/cis.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']      = 'Apmokėjimas parduotuvėje';
+
+// Text
+$_['text_payment']       = 'Apkokėjimas';
+$_['text_success']       = 'Pavyko: Jūs sėkmingai redagavote nustatymus!';
+
+// Entry
+$_['entry_total']        = 'Iš viso:<br /><span class="help">Saugumo sumetimais, suma kuri turi būti, norint atsiimti parduotuvėje (Standartas 0.00).</span>';
+$_['entry_order_status'] = 'Užsakymo statusas:';
+$_['entry_geo_zone']     = 'Gegrafinė zona:<br /><span class="help">Miestas, kuriame galima atsiimti prekes (Pvz.: Vilnius), arba palikti visą Lietuvos zoną.</span>';
+$_['entry_status']       = 'Statusas:';
+$_['entry_sort_order']   = 'Eilės numeris:';
+
+// Error
+$_['error_permission']   = 'Dėmesio: Jūs neturite teisių redaguoti modulio!';
+?>
---
+++ Moduliai/payment_CIS/catalog/language/english/payment/cis.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_title'] = 'Cash In Shop';
+?>
